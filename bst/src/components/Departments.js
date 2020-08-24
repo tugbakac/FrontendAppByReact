65919fefc301f5519ef575aa2a3a89c844aa1c84
tugbakac <tugbakac45@gmail.com>
2020-08-25 01:51:19 +0300

    second commit

---
+++ bst/src/components/Departments.js
@@ -0,0 +1,13 @@
+import React, {Components, Component} from 'react';
+
+export class Departments extends Component{
+    render(){
+        return(
+            <div>
+                <h3>
+                    Welcome to Departments Page
+                </h3>
+            </div>
+        )
+    }
+}
