e7d6038bf27331fbc10dfaff280d129a3f12ea28
tugbakac <tugbakac45@gmail.com>
2020-08-27 01:12:35 +0300

    third commit

--- bst/src/App.js
+++ bst/src/App.js
@@ -6,15 +6,23 @@ import {Home} from './components/Home';
 import {Departments} from './components/Departments';
 import {Employees} from './components/Employees';
 import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import {Navigation} from './components/Navigation';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <h3 className="m-3 d-flex justify-content-center">
+      React Js with Bootstrap
+    </h3>
+    <h5 className="m-3 d-flex justify-content-center">
+      Employee Management Portal 
+    </h5>
+    <Navigation/>
       <Switch>
         <Route path='/' component={Home} exact/>
-        <Route path='/department' component={Departments} exact/>
-        <Route path='/employee' component={Employees} exact />
+        <Route path='/department' component={Departments} />
+        <Route path='/employee' component={Employees} />
       </Switch>
     </BrowserRouter>
   );
--- bst/src/components/Departments.js
+++ bst/src/components/Departments.js
@@ -1,13 +1,47 @@
 import React, {Components, Component} from 'react';
+import { Table } from 'react-bootstrap';
 
 export class Departments extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.state={
+            deps:[]
+        };
+    }
+    componentDidMount(){
+        this.refleshlist();
+    }
+    refleshlist()
+    {
+        this.setState({
+                deps:[
+                    {"DepartmentID":1,"DepartmentName":"IT"},
+                    {"DepartmentID":2,"DepartmentName":"Support"}
+                ]
+            });
+    }
     render(){
+        const{deps}=this.state;
         return(
-            <div>
-                <h3>
-                    Welcome to Departments Page
-                </h3>
-            </div>
+            <Table className="mt-4" striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>DepartmentID</th>
+                        <th>DepartmentName</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        deps.map(dep=>
+                            <tr key={dep.DepartmentID}>
+                                <td>{dep.DepartmentID}</td>
+                                <td>{dep.DepartmentName}</td>
+                            </tr>
+                            )
+                    }
+                </tbody>
+            </Table>
         )
     }
 }
