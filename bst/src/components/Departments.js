e8ba33aec56d14d7e32b77b44a6a2b223f467148
tugbakac <tugbakac45@gmail.com>
2020-09-21 16:37:48 +0300

    third commit

--- bst/src/components/Departments.js
+++ bst/src/components/Departments.js
@@ -1,34 +1,51 @@
-import React, {Components, Component} from 'react';
+import React, { Component} from 'react';
 import { Table } from 'react-bootstrap';
+import { AddDepModal } from './AddDepModal';
+import {EditDepModal} from './EditDepModal';
+import { Button,ButtonToolbar } from 'react-bootstrap';
 
 export class Departments extends Component{
     constructor(props)
     {
         super(props);
-        this.state={
-            deps:[]
+        this.state=
+        {
+            deps:[],
+            addModalShow:false,
+            editModalShow:false
         };
     }
     componentDidMount(){
         this.refleshlist();
     }
+
+    componentDidUpdate(){
+        this.refleshlist();
+    }
+
+    //connection with API
     refleshlist()
     {
-        this.setState({
-                deps:[
-                    {"DepartmentID":1,"DepartmentName":"IT"},
-                    {"DepartmentID":2,"DepartmentName":"Support"}
-                ]
+        fetch('http://localhost:58127/api/Department')
+            .then(response => response.json())
+            .then(data =>{
+                this.setState({
+                    deps:data
+                });
             });
     }
     render(){
-        const{deps}=this.state;
+        const{deps,depID,depName}=this.state;
+        let addModalClose=()=>this.setState({addModalShow:false})
+        let editModalClose=()=>this.setState({editModalShow:false})
         return(
-            <Table className="mt-4" striped bordered hover size="sm">
+            <div>
+                    <Table className="mt-4" striped bordered hover size="sm">
                 <thead>
                     <tr>
                         <th>DepartmentID</th>
                         <th>DepartmentName</th>
+                        <th>Option</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,11 +54,37 @@ export class Departments extends Component{
                             <tr key={dep.DepartmentID}>
                                 <td>{dep.DepartmentID}</td>
                                 <td>{dep.DepartmentName}</td>
+                                <td>
+                                    <ButtonToolbar>
+                                        <Button variant="warning text-light" onClick={()=>this.setState({
+                                            editModalShow:true,
+                                            depID:dep.DepartmentID,
+                                            depName:dep.DepartmentName
+                                        })}>
+                                            Edit
+                                        </Button>
+                                        <EditDepModal
+                                            show={this.state.editModalShow}
+                                            onHide={editModalClose}
+                                            depID={depID}
+                                            depName={depName}
+                                        />
+                                    </ButtonToolbar>
+                                </td>
                             </tr>
                             )
                     }
                 </tbody>
             </Table>
+            <ButtonToolbar>
+                    <Button onClick={() => this.setState({
+                        addModalShow:true
+                    })}>
+                        Add Department
+                    </Button>
+            </ButtonToolbar>
+            <AddDepModal show={this.state.addModalShow} onHide={addModalClose} />
+            </div>
         )
     }
 }
